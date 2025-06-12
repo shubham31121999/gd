b6ed5758a6e128f94fa5bca7b911f4b587123cad
Shubham Gajanan Uparkar <65241730+shubham31121999@users.noreply.github.com>
2025-06-12 19:03:07 +0530

    Done

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,16 +7,20 @@ function Banner() {
     <div className="w-full overflow-hidden">
       {/* Desktop Image */}
       <img
+      rel="preload"
         src={desktopBanner}
         alt="Desktop Banner"
         className="hidden sm:block w-full h-full object-cover"
+        fetchpriority="high"
       />
 
       {/* Mobile Image */}
       <img
+      rel="preload"
         src={mobileBanner}
         alt="Mobile Banner"
         className="block sm:hidden w-full h-full object-cover"
+        fetchpriority="high"
       />
     </div>
   );
