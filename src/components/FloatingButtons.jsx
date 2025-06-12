36e593438baba5c9aa39ba66400bfd435ba41e5a
shubham31121999 <65241730+shubham31121999@users.noreply.github.com>
2025-06-13 02:27:52 +0530

    Done

--- src/components/FloatingButtons.jsx
+++ src/components/FloatingButtons.jsx
@@ -10,7 +10,7 @@ function FloatingButtons() {
     }
   };
   return (
-    <div className="fixed bottom-4 left-4 z-50 flex flex-col items-center gap-3">
+    <div className="fixed bottom-4 right-4 z-50 flex flex-col items-center gap-3">
       {/* Call Button */}
       <a
         href="tel:7045117623"
